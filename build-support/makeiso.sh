cbd5633b470b55f2cf387d39c2cab800bb31d4d3
streaksu <streaksu@protonmail.com>
2023-02-26 22:57:52 +0100

    ports: Update mlibc, add neofetch to replace ufetch, replace showmem with util-ironclad, update ironclad and gcon

--- build-support/makeiso.sh
+++ build-support/makeiso.sh
@@ -8,25 +8,22 @@ rm -rf sysroot
 ./jinx host-build limine
 ./jinx host-build memtest86+
 
-# Make an initramfs with the sysroot.
-(cd sysroot && tar cvf ../initramfs.tar *)
-
-# TODO: Once ready, move to ext4 like this.
-# rm -rf initramfs mount_contents
-# mkdir mount_contents
-# fallocate -l 400M initramfs
-# sudo mkfs.ext4 initramfs
-# sudo losetup /dev/loop9 initramfs
-# sudo mount /dev/loop9 mount_contents
-# sudo cp -r sysroot/* mount_contents/
-# sudo umount mount_contents
-# sudo losetup -D /dev/loop9
+# TODO: Once ready, move to ext4, now its ext2 only.
+rm -rf initramfs mount_contents
+mkdir mount_contents
+fallocate -l 400M initramfs
+sudo mkfs.ext2 initramfs
+sudo losetup /dev/loop9 initramfs
+sudo mount /dev/loop9 mount_contents
+sudo cp -r sysroot/* mount_contents/
+sudo umount mount_contents
+sudo losetup -D /dev/loop9
 
 # Prepare the iso and boot directories.
 rm -rf iso_root
 mkdir -pv iso_root/boot
 cp -r artwork/background.bmp iso_root/boot/
-cp -r initramfs.tar          iso_root/boot/
+cp -r initramfs              iso_root/boot/
 cp -r sysroot/boot/*         iso_root/boot/
 
 # Install the limine and memtest86+ binaries.

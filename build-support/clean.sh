190fab66e77c8382ce263c41692a950357654c53
streaksu <streaksu@mailbox.org>
2024-11-17 19:44:50 +0100

    build-support: support sudo replacements and add clean

---
+++ build-support/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -ex
+
+# Let the user pass their own sudo (or doas).
+: "${SUDO:=sudo}"
+
+if [ "$1" = "--erase-cache" ]; then
+    $SUDO rm -rf .jinx-cache
+fi
+
+$SUDO rm -rf gloire.img sysroot sources builds pkgs host-builds host-pkgs
--- build-support/makeiso.sh
+++ build-support/makeiso.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
+# Let the user pass their own $SUDO (or doas).
+: "${SUDO:=sudo}"
+
 # Build the sysroot with jinx, and make sure the packages the particular
 # target needs.
 set -f
-sudo rm -rf sysroot
+$SUDO rm -rf sysroot
 ./jinx install "sysroot" base $PKGS_TO_INSTALL
 set +f
 if [ "$JINX_CONFIG_FILE" = "jinx-config-riscv64" ]; then
@@ -22,49 +25,49 @@ else
 fi
 
 # Ensure permissions are set.
-sudo chown -R root:root sysroot/*
-sudo chown -R 1000:1000 sysroot/home/user
-sudo chmod 700 sysroot/root
-sudo chmod 777 sysroot/tmp
+$SUDO chown -R root:root sysroot/*
+$SUDO chown -R 1000:1000 sysroot/home/user
+$SUDO chmod 700 sysroot/root
+$SUDO chmod 777 sysroot/tmp
 
 # TODO: Once ready, move to ext4, now its ext2 only.
 rm -f gloire.img
 fallocate -l 2G gloire.img
-sudo parted -s gloire.img mklabel gpt
-sudo parted -s gloire.img mkpart ESP fat32 2048s 5%
-sudo parted -s gloire.img mkpart gloire_data ext2 5% 100%
-sudo parted -s gloire.img set 1 esp on
-sudo sgdisk gloire.img -u 1:f2ac3998-bd9d-4193-bb75-efcd9e90dd96
-sudo sgdisk gloire.img -u 2:123e4567-e89b-12d3-a456-426614174000
+$SUDO parted -s gloire.img mklabel gpt
+$SUDO parted -s gloire.img mkpart ESP fat32 2048s 5%
+$SUDO parted -s gloire.img mkpart gloire_data ext2 5% 100%
+$SUDO parted -s gloire.img set 1 esp on
+$SUDO sgdisk gloire.img -u 1:f2ac3998-bd9d-4193-bb75-efcd9e90dd96
+$SUDO sgdisk gloire.img -u 2:123e4567-e89b-12d3-a456-426614174000
 
-LOOPBACK_DEV=$(sudo losetup -Pf --show gloire.img)
-sudo mkfs.fat -F 32 ${LOOPBACK_DEV}p1
-sudo mkfs.ext2 ${LOOPBACK_DEV}p2
+LOOPBACK_DEV=$($SUDO losetup -Pf --show gloire.img)
+$SUDO mkfs.fat -F 32 ${LOOPBACK_DEV}p1
+$SUDO mkfs.ext2 ${LOOPBACK_DEV}p2
 mkdir -p mount_dir
-sudo mount ${LOOPBACK_DEV}p2 mount_dir
+$SUDO mount ${LOOPBACK_DEV}p2 mount_dir
 
-sudo cp -rp sysroot/* mount_dir/
-sudo rm -rf mount_dir/boot
-sudo mkdir -p mount_dir/boot
-sudo mount ${LOOPBACK_DEV}p1 mount_dir/boot
+$SUDO cp -rp sysroot/* mount_dir/
+$SUDO rm -rf mount_dir/boot
+$SUDO mkdir -p mount_dir/boot
+$SUDO mount ${LOOPBACK_DEV}p1 mount_dir/boot
 
 # Prepare the iso and boot directories.
-sudo cp -r artwork/background.bmp              mount_dir/boot/
-sudo cp -r sysroot/usr/share/ironclad/ironclad mount_dir/boot/
+$SUDO cp -r artwork/background.bmp              mount_dir/boot/
+$SUDO cp -r sysroot/usr/share/ironclad/ironclad mount_dir/boot/
 
 # Install the boot binaries required by the target.
 if [ "$JINX_CONFIG_FILE" = "jinx-config-riscv64" ]; then
-    sudo mkdir -pv mount_dir/boot/EFI/BOOT
-    sudo cp -r host-pkgs/limine/usr/local/share/limine/limine-uefi-cd.bin mount_dir/boot/
-    sudo cp host-pkgs/limine/usr/local/share/limine/BOOTRISCV64.EFI       mount_dir/boot/EFI/BOOT/
+    $SUDO mkdir -pv mount_dir/boot/EFI/BOOT
+    $SUDO cp -r host-pkgs/limine/usr/local/share/limine/limine-uefi-cd.bin mount_dir/boot/
+    $SUDO cp host-pkgs/limine/usr/local/share/limine/BOOTRISCV64.EFI       mount_dir/boot/EFI/BOOT/
 else
-    sudo mkdir -pv mount_dir/boot/EFI/BOOT
-    sudo cp -r host-pkgs/limine/usr/local/share/limine/limine-bios.sys    mount_dir/boot/
-    sudo cp -r host-pkgs/limine/usr/local/share/limine/limine-bios-cd.bin mount_dir/boot/
-    sudo cp -r host-pkgs/limine/usr/local/share/limine/limine-uefi-cd.bin mount_dir/boot/
-    sudo cp host-pkgs/limine/usr/local/share/limine/BOOTX64.EFI           mount_dir/boot/EFI/BOOT/
-    sudo cp host-pkgs/limine/usr/local/share/limine/BOOTIA32.EFI          mount_dir/boot/EFI/BOOT/
-    sudo cp -r host-pkgs/memtest86+/boot/memtest.bin                      mount_dir/boot/
+    $SUDO mkdir -pv mount_dir/boot/EFI/BOOT
+    $SUDO cp -r host-pkgs/limine/usr/local/share/limine/limine-bios.sys    mount_dir/boot/
+    $SUDO cp -r host-pkgs/limine/usr/local/share/limine/limine-bios-cd.bin mount_dir/boot/
+    $SUDO cp -r host-pkgs/limine/usr/local/share/limine/limine-uefi-cd.bin mount_dir/boot/
+    $SUDO cp host-pkgs/limine/usr/local/share/limine/BOOTX64.EFI           mount_dir/boot/EFI/BOOT/
+    $SUDO cp host-pkgs/limine/usr/local/share/limine/BOOTIA32.EFI          mount_dir/boot/EFI/BOOT/
+    $SUDO cp -r host-pkgs/memtest86+/boot/memtest.bin                      mount_dir/boot/
 fi
 
 # Generate the config file. Take into account that there may not be a graphical
@@ -132,13 +135,13 @@ if [ -z "$JINX_CONFIG_FILE" ]; then # Assume its only defined for riscv64.
 EOF
 fi
 
-sudo cp "$CONFIG_TEMP" mount_dir/boot/limine.conf
+$SUDO cp "$CONFIG_TEMP" mount_dir/boot/limine.conf
 
 # Unmount after we are done.
 sync
-sudo umount -R mount_dir
-sudo rm -rf mount_dir
-sudo losetup -d ${LOOPBACK_DEV}
+$SUDO umount -R mount_dir
+$SUDO rm -rf mount_dir
+$SUDO losetup -d ${LOOPBACK_DEV}
 
 # Post installation triggers on the whole image.
 if [ "$JINX_CONFIG_FILE" = "jinx-config-riscv64" ]; then

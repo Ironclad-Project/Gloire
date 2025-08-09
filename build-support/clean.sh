08b6ca7ee0c482ac22585423fbe2dffe9c407a0b
Mintsuki <mintsuki@protonmail.com>
2025-08-09 08:27:52 +0200

    build: Update clean.sh script

--- build-support/clean.sh
+++ build-support/clean.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+script_dir="$(dirname "$0")"
+test -z "${script_dir}" && script_dir=.
+
+source_dir="$(cd "${script_dir}"/.. && pwd -P)"
+
+cd "${source_dir}"
+
 # Let the user pass their own sudo (or doas).
 : "${SUDO:=sudo}"
 
@@ -9,4 +16,4 @@ if [ "$1" = "--erase-cache" ]; then
     $SUDO rm -rf .jinx-cache
 fi
 
-$SUDO rm -rf gloire.iso gloire.img sysroot sources builds pkgs host-builds host-pkgs
+$SUDO rm -rf build-x86_64 build-riscv64 sources

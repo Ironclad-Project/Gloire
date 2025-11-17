a7e74b265b1bd164d428a21eb5c553c737e4fc4f
Mintsuki <mintsuki@protonmail.com>
2025-11-17 01:09:13 +0100

    misc: Update checksysroot.sh to be clearer

--- build-support/checksysroot.sh
+++ build-support/checksysroot.sh
@@ -8,15 +8,16 @@ for f in pkgs/*; do
         PRINT_PKG=0
         zstdcat < "$PKGPATH" | tar -xf -
         for ff in $(find . -type f); do
-            stuff="$(strings "$ff" | grep '/sysroot' | grep -v Assertion | grep -v '\--enable-languages')"
+            stuff="$(strings "$ff" | grep '/sysroot' | sort | uniq)"
             if [ -z "$stuff" ]; then
                 continue
             fi
-            echo "    $ff"
+            printf "\e[44;97m------- vvv ------- $ff ------- vvv -------\e[m\n"
+            echo "$stuff"
             PRINT_PKG=1
         done
         if [ "$PRINT_PKG" = 1 ]; then
-            echo "$f"
+            printf "\e[46;97m------- ^^^ ------- $f ------- ^^^ -------\e[m\n"
         fi
     )
     rm -rf "$TMPDIR"

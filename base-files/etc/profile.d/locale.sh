66221480ce7f614642bb35f25b87f1c99f81613b
streaksu <streaksu@mailbox.org>
2026-01-27 01:01:07 +0100

    base-files: add better locale instrumentation
    Signed-off-by: streaksu <streaksu@mailbox.org>

---
+++ base-files/etc/profile.d/locale.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Load locale.conf if present in any XDG paths.
+if [ -z "$LANG" ]; then
+  if [ -n "$XDG_CONFIG_HOME" ] && [ -r "$XDG_CONFIG_HOME/locale.conf" ]; then
+    . "$XDG_CONFIG_HOME/locale.conf"
+  elif [ -n "$HOME" ] && [ -r "$HOME/.config/locale.conf" ]; then
+    . "$HOME/.config/locale.conf"
+  elif [ -r /etc/locale.conf ]; then
+    . /etc/locale.conf
+  fi
+fi
+
+# define default LANG to C.UTF-8 if not already defined
+LANG=${LANG:-C.UTF-8}
+
+# export all locale variables when they exist
+export LANG LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY \
+       LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT \
+       LC_IDENTIFICATION
